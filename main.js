cd956bd9fd64576b1c6609d3acebd835e8b31787
am1k <vitaliy_bog@mail.ru>
2015-08-03 18:50:16 +0300

    add

--- main.js
+++ main.js
@@ -2,66 +2,154 @@
  * Created by v.bogoroditskiy on 7/31/2015.
  */
 
+var currentElement,
+    currentElements,
+    currentElementIndex,
+    nextButton = document.querySelector('.selector-next')
+    nextSiblingBtn = document.querySelector('.nav-right');
+
+var i = 0;
+
+//document.querySelector('.selector-find').addEventListener('click', findSelector);
 
 function findSelector(){
     var selectorName = document.querySelector(".selector").value;
+    currentElementIndex = 0;
     if(selectorName !== null || undefined) {
-        var currentBlock = document.querySelector(selectorName);
-        currentBlock.classList.toggle('active');
+        currentElements = document.querySelectorAll(selectorName);
+        currentElement = currentElements[currentElementIndex];
+        refreshButtons();
     }
-    return currentBlock;
 }
 
-/*function nextSelector() {
-    var text = document.querySelector(".selector").value;
-    if(text !== null || undefined) {
-       var nextS = text.nextSibling;
-           nextS.classList.add("active");
-    }
-}*/
 function allElements() {
     var selectorName = document.querySelector(".selector").value;
-    var elements = document.getElementsByTagName(selectorName);
-    var array = [];
-    for (var i = 0; i < elements.length; i++) {
-        array.push(elements[i]);
+    return Array.prototype.slice.call(document.getElementsByTagName(selectorName), 0);
+}
+
+function next(array){
+    currentElementIndex++;
+    currentElement = currentElements[currentElementIndex];
+    refreshNextButton();
+}
+
+function prevButton(array){
+    var nextSelector = allElements(array);
+    if (i > nextSelector.length-1) {
+        i = 0;
+
+    }else {
+        currentElementIndex = nextSelector[i];
+        //nextSelector[i].classList.add('prev');
+        i--;
     }
-    return array;
+    return nextSelector;
 }
 
-
-var i = 0;
-function nextElement(array) {
-    //if (i == allElements(array).length -1) {
-    //    i = 0;
-    //    allElements(array).classList.add('active');
-    //} else {
-    //    i++;
-    //}
-    var nextSelector = allElements(array)[i++ % allElements(array).length];
-    nextSelector.classList.add('active');
-}
-function prevElement(array) {
-    var nextSelector = allElements(array)[i-- % allElements(array).length];
-    nextSelector.classList.add('prev');
+function parentNode() {
+    currentElement = currentElement.parentNode;
+    console.log(currentElement);
 }
 
-function getParent(currentBlock){
-    var parentBlock = findSelector(currentBlock).parentNode;
-    parentBlock.classList.add('active');
+function firstChild() {
+    currentElement = currentElement.firstElementChild;
+    console.log(currentElement);
 }
 
-function firstChildren(currentBlock){
-    var firstChild = findSelector(currentBlock).firstElementChild;
-    firstChild.classList.add('active');
+function nextElementSibling(btn) {
+    currentElement = currentElement.nextElementSibling;
+    refreshNextSiblingButton();
+    console.log(currentElement, 'next');
+
 }
 
-function nextElementSibling(currentBlock) {
-    var nextElementSib = findSelector(currentBlock).nextElementSibling ;
-    nextElementSib.classList.add('active');
+function prevElementSibling() {
+    currentElement = currentElement.previousElementSibling;
+    console.log(currentElement);
 }
 
-function prevElementSibling(currentBlock) {
-    var prevElementSib = findSelector(currentBlock).previousElementSibling ;
-    prevElementSib.classList.add('active');
+//document.querySelector('.selector-find').addEventListener('click', enableButton);
+
+function enableButton(array) {
+
+    if(nextButton(array).length > 2){
+        document.querySelector('.selector-next').disabled = false;
+    } else {
+        document.querySelector('.selector-next').disabled = true;
+    }
+    if(prevButton(array).length > 2){
+        document.querySelector('.selector-prev').disabled = false;
+    } else {
+        document.querySelector('.selector-prev').disabled = true;
+    }
+
+}
+
+document.querySelector('.jsbursa-panel').addEventListener('click', function(e) {
+    if (e.srcElement.tagName.toLocaleLowerCase() === 'button'){
+        if(currentElement){
+            currentElement.classList.remove('active');
+            console.log('remove', currentElement);
+        }
+
+        /*currentElementIndex && currentElementIndex.classList.remove('active');
+        currentElementIndex && currentElementIndex.classList.remove('prev');*/
+
+        if(e.srcElement.className.indexOf('selector-find') !== -1){
+            findSelector(e.srcElement.value);
+        }
+        if(e.srcElement.className.indexOf('selector-next') !== -1) {
+            next(e.srcElement.value);
+        }
+        if(e.srcElement.className.indexOf('selector-prev') !== -1) {
+            prevButton(e.srcElement.value);
+        }
+        if(e.srcElement.className.indexOf('nav-top') !== -1){
+            parentNode(e.srcElement.value);
+        }
+        if(e.srcElement.className.indexOf('nav-bottom') !== -1){
+            firstChild(e.srcElement.value);
+        }
+        if(e.srcElement === nextSiblingBtn){
+            nextElementSibling(e.srcElement);
+        }
+        if(e.srcElement.className.indexOf('nav-left') !== -1){
+            prevElementSibling(e.srcElement.value);
+        }
+
+        console.log(currentElement)
+        currentElement.classList.add('active');
+
+    }
+});
+
+
+function refreshButtons(){
+    refreshNextSiblingButton();
+    refreshNextButton();
+}
+
+function refreshNextButton(){
+    if(hasNext()){
+        nextButton.disabled = false;
+    }else{
+        nextButton.disabled = true;
+    }
+}
+
+function refreshNextSiblingButton(){
+    if(hasNextSibling()){
+        nextSiblingBtn.disabled = false;
+    }else {
+        nextSiblingBtn.disabled = true;
+    }
+}
+
+function hasNextSibling(){
+    return currentElement.nextElementSibling;
+}
+
+function hasNext(){
+    console.log(currentElementIndex, currentElements.length)
+    return currentElements && currentElementIndex < currentElements.length - 1;
 }
