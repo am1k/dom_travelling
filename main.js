9d70931bf6169470638e9ff23fed97161465848d
am1k <vitaliy_bog@mail.ru>
2015-08-05 12:50:58 +0300

    version prototype

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 
 
 function DOMObservable(){
+    this.currentElement = null;
     this.init();
 }
 
@@ -21,38 +22,36 @@ DOMObservable.prototype = {
         this.nextSiblingBtn = document.querySelector('.nav-right');
     },
     addHandlers: function() {
+        var self = this;
         document.querySelector('.jsbursa-panel').addEventListener('click', function(e) {
             if (e.srcElement.tagName.toLocaleLowerCase() === 'button'){
-                if(currentElement){
-                    currentElement.classList.remove('active');
-                    console.log('remove', currentElement);
-                }
 
+                if(self.currentElement){
+                    self.currentElement.classList.remove('active');
+                }
                 if(e.srcElement.className.indexOf('selector-find') !== -1){
-                    findSelector(e.srcElement.value);
+                    self.findSelector(e.srcElement.value);
                 }
-                if(e.srcElement === nextButton) {
-                    console.log(this.next);
-                    next(e.srcElement);
+                if(e.srcElement === self.nextButton) {
+                    self.next(e.srcElement);
                 }
-                if(e.srcElement === prevButton) {
-                    prev(e.srcElement);
+                if(e.srcElement === self.prevButton) {
+                    self.prev(e.srcElement);
                 }
-                if(e.srcElement === parentNodeBtn){
-                    parentNode(e.srcElement);
+                if(e.srcElement === self.parentNodeBtn){
+                    self.parentNode(e.srcElement);
                 }
-                if(e.srcElement === firstChildBtn){
-                    firstChild(e.srcElement);
+                if(e.srcElement === self.firstChildBtn){
+                    self.firstChild(e.srcElement);
                 }
-                if(e.srcElement === nextSiblingBtn){
-                    nextElementSibling(e.srcElement);
+                if(e.srcElement === self.nextSiblingBtn){
+                    self.nextElementSibling(e.srcElement);
                 }
-                if(e.srcElement === prevSiblingBtn){
-                    prevElementSibling(e.srcElement);
+                if(e.srcElement === self.prevSiblingBtn){
+                    self.prevElementSibling(e.srcElement);
                 }
-
-                currentElement.classList.add('active');
-                refreshButtons();
+                self.currentElement.classList.add('active');
+                self.refreshButtons();
             }
         });
     },
@@ -61,16 +60,16 @@ DOMObservable.prototype = {
         this.currentElementIndex = 0;
         if(selectorName !== null || undefined) {
             this.currentElements = document.querySelectorAll(selectorName);
-            this.currentElement = this.currentElements[currentElementIndex];
+            this.currentElement = this.currentElements[this.currentElementIndex];
         }
     },
     next: function(array){
         this.currentElementIndex++;
-        this.currentElement = this.currentElements[currentElementIndex];
+        this.currentElement = this.currentElements[this.currentElementIndex];
     },
     prev: function(array){
         this.currentElementIndex--;
-        this.currentElement = this.currentElements[currentElementIndex];
+        this.currentElement = this.currentElements[this.currentElementIndex];
     },
     parentNode: function(btn){
         this.currentElement = this.currentElement.parentNode;
@@ -85,236 +84,71 @@ DOMObservable.prototype = {
         this.currentElement = this.currentElement.previousElementSibling;
     },
     refreshButtons: function(){
-        this.refreshNextSiblingButton;
-        this.refreshPrevSiblingButton;
-        this.refreshNextButton;
-        this.refreshPrevButton;
-        this.refreshParentNodeButton;
-        this.refreshFirstChildButton;
+        this.refreshNextSiblingButton();
+        this.refreshPrevSiblingButton();
+        this.refreshNextButton();
+        this.refreshPrevButton();
+        this.refreshParentNodeButton();
+        this.refreshFirstChildButton();
     },
     refreshNextButton: function(){
         if(this.hasNext()){
-            nextButton.disabled = false;
+            this.nextButton.disabled = false;
         }else{
-            nextButton.disabled = true;
+            this.nextButton.disabled = true;
         }
     },
     refreshPrevButton: function(){
         if(this.hasPrev()){
-            prevButton.disabled = false;
+            this.prevButton.disabled = false;
         }else {
-            prevButton.disabled = true;
+            this.prevButton.disabled = true;
         }
     },
     refreshNextSiblingButton: function(){
         if(this.hasNextSibling()){
-            nextSiblingBtn.disabled = false;
+            this.nextSiblingBtn.disabled = false;
         }else {
-            nextSiblingBtn.disabled = true;
+            this.nextSiblingBtn.disabled = true;
         }
     },
     refreshPrevSiblingButton: function(){
         if(this.hasPrevSibling()){
-            prevSiblingBtn.disabled = false;
+            this.prevSiblingBtn.disabled = false;
         }else {
-            prevSiblingBtn.disabled = true;
+            this.prevSiblingBtn.disabled = true;
         }
     },
     refreshParentNodeButton: function() {
         if(this.hasParentNode()){
-            parentNodeBtn.disabled = false;
+            this.parentNodeBtn.disabled = false;
         }else {
-            parentNodeBtn.disabled = true;
+            this.parentNodeBtn.disabled = true;
         }
     },
     refreshFirstChildButton: function() {
         if(this.hasFirstChild()){
-            firstChildBtn.disabled = false;
+            this.firstChildBtn.disabled = false;
         }else {
-            firstChildBtn.disabled = true;
+            this.firstChildBtn.disabled = true;
         }
     },
     hasFirstChild: function() {
-        return currentElement.firstElementChild;
+        return this.currentElement.firstElementChild;
     },
     hasNextSibling: function(){
-        return currentElement.nextElementSibling;
+        return this.currentElement.nextElementSibling;
     },
     hasPrevSibling: function(){
-        return currentElement.previousElementSibling;
+        return this.currentElement.previousElementSibling;
     },
     hasParentNode: function(){
-        return currentElement.parentNode;
+        return this.currentElement.parentNode;
     },
     hasNext: function(){
-        return currentElements && currentElementIndex < currentElements.length - 1;
+        return this.currentElements && this.currentElementIndex < this.currentElements.length - 1;
     },
     hasPrev: function(){
-        return currentElements && currentElementIndex > 0 ;
+        return this.currentElements && this.currentElementIndex > 0 ;
     }
 };
-
-
-
-
-//document.querySelector('.selector-find').addEventListener('click', findSelector);
-
-//function findSelector(){
-//    var selectorName = document.querySelector(".selector").value;
-//    currentElementIndex = 0;
-//    if(selectorName !== null || undefined) {
-//        currentElements = document.querySelectorAll(selectorName);
-//        currentElement = currentElements[currentElementIndex];
-//    }
-//}
-//
-//function next(array){
-//    currentElementIndex++;
-//    currentElement = currentElements[currentElementIndex];
-//}
-//
-//function prev(array){
-//    currentElementIndex--;
-//    currentElement = currentElements[currentElementIndex];
-//
-//}
-//
-//function parentNode(btn) {
-//    currentElement = currentElement.parentNode;
-//    console.log(currentElement);
-//}
-//
-//function firstChild(btn) {
-//    currentElement = currentElement.firstElementChild;
-//    console.log(currentElement);
-//}
-//
-//function nextElementSibling(btn) {
-//    currentElement = currentElement.nextElementSibling;
-//    console.log(currentElement, 'next');
-//
-//}
-//function prevElementSibling(btn) {
-//    currentElement = currentElement.previousElementSibling;
-//    console.log(currentElement, 'prev');
-//}
-
-//document.querySelector('.jsbursa-panel').addEventListener('click', function(e) {
-//    if (e.srcElement.tagName.toLocaleLowerCase() === 'button'){
-//        if(currentElement){
-//            currentElement.classList.remove('active');
-//            console.log('remove', currentElement);
-//        }
-//
-//        if(e.srcElement.className.indexOf('selector-find') !== -1){
-//            findSelector(e.srcElement.value);
-//        }
-//        if(e.srcElement === nextButton) {
-//            console.log(this.next);
-//            next(e.srcElement);
-//        }
-//        if(e.srcElement === prevButton) {
-//            prev(e.srcElement);
-//        }
-//        if(e.srcElement === parentNodeBtn){
-//            parentNode(e.srcElement);
-//        }
-//        if(e.srcElement === firstChildBtn){
-//            firstChild(e.srcElement);
-//        }
-//        if(e.srcElement === nextSiblingBtn){
-//            nextElementSibling(e.srcElement);
-//        }
-//        if(e.srcElement === prevSiblingBtn){
-//            prevElementSibling(e.srcElement);
-//        }
-//
-//        currentElement.classList.add('active');
-//        refreshButtons();
-//    }
-//});
-
-
-//function refreshButtons(){
-//    refreshNextSiblingButton();
-//    refreshPrevSiblingButton();
-//    refreshNextButton();
-//    refreshPrevButton();
-//    refreshParentNodeButton();
-//    refreshFirstChildButton();
-//}
-
-//function refreshNextButton(){
-//    if(hasNext()){
-//        nextButton.disabled = false;
-//    }else{
-//        nextButton.disabled = true;
-//    }
-//}
-//
-//function refreshPrevButton(){
-//    if(hasPrev()){
-//        prevButton.disabled = false;
-//    }else {
-//        prevButton.disabled = true;
-//    }
-//}
-//
-//function refreshNextSiblingButton(){
-//    if(hasNextSibling()){
-//        nextSiblingBtn.disabled = false;
-//    }else {
-//        nextSiblingBtn.disabled = true;
-//    }
-//}
-//
-//function refreshPrevSiblingButton(){
-//    if(hasPrevSibling()){
-//        prevSiblingBtn.disabled = false;
-//    }else {
-//        prevSiblingBtn.disabled = true;
-//    }
-//}
-//
-//function refreshParentNodeButton() {
-//    if(hasParentNode()){
-//        parentNodeBtn.disabled = false;
-//    }else {
-//        parentNodeBtn.disabled = true;
-//    }
-//}
-//
-//function refreshFirstChildButton() {
-//    if(hasFirstChild()){
-//        firstChildBtn.disabled = false;
-//    }else {
-//        firstChildBtn.disabled = true;
-//    }
-//}
-//
-//function hasFirstChild() {
-//    return currentElement.firstElementChild;
-//}
-//
-//function hasNextSibling(){
-//    return currentElement.nextElementSibling;
-//}
-//
-//function hasPrevSibling(){
-//    return currentElement.previousElementSibling;
-//}
-//
-//function hasParentNode(){
-//    return currentElement.parentNode;
-//}
-//
-//function hasNext(){
-//    console.log(currentElementIndex, currentElements.length);
-//    return currentElements && currentElementIndex < currentElements.length - 1;
-//}
-//
-//function hasPrev(){
-//    console.log(currentElementIndex, currentElements.length);
-//    return currentElements && currentElementIndex > 0 ;
-//}
-
